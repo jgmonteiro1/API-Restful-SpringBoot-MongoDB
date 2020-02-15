68c2d6695b149e5d2c7d587b07315247fe3f10e6
João Gabriel <jgmoreira15@gmail.com>
2020-02-15 01:59:40 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/jgmonteiro/course/Course2Application.java
@@ -0,0 +1,13 @@
+package com.jgmonteiro.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2Application.class, args);
+	}
+
+}
