c36e075c1e2ff1adbffca38d01577d9e9be180ea
João Gabriel <jgmoreira15@gmail.com>
2020-02-16 01:28:33 -0300

    Usando padrão DTO para retornar usuários

---
+++ src/main/java/com/jgmonteiro/course/dto/UserDTO.java
@@ -0,0 +1,52 @@
+package com.jgmonteiro.course.dto;
+
+import java.io.Serializable;
+
+import com.jgmonteiro.course.domain.User;
+
+public class UserDTO implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+		
+	}
+	
+	//Para automatizar a instanciação de um userDTO a partir de um User
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
