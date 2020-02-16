21d4d0c8b8d20a72415ba280dfa8bc56212741e7
João Gabriel <jgmoreira15@gmail.com>
2020-02-16 03:28:13 -0300

    Implementação do método findById + tratamentos de exceptions referentes a objeto não encontrado

--- src/main/java/com/jgmonteiro/course/services/UserService.java
+++ src/main/java/com/jgmonteiro/course/services/UserService.java
@@ -1,12 +1,14 @@
 package com.jgmonteiro.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jgmonteiro.course.domain.User;
 import com.jgmonteiro.course.repository.UserRepository;
+import com.jgmonteiro.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/jgmonteiro/course/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.jgmonteiro.course.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
