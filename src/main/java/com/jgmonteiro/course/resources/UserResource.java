036d46674b22598765ca00a29eaedf0a212487a0
João Gabriel <jgmoreira15@gmail.com>
2020-02-22 00:16:49 -0300

    Inserção de usuário com POST, deleção de usuário com DELETE e atualizaçao de usuário com PUT

--- src/main/java/com/jgmonteiro/course/resources/UserResource.java
+++ src/main/java/com/jgmonteiro/course/resources/UserResource.java
@@ -1,15 +1,20 @@
 package com.jgmonteiro.course.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jgmonteiro.course.domain.User;
 import com.jgmonteiro.course.dto.UserDTO;
@@ -33,5 +38,28 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		//Convertendo o objDto para user 
+		User obj = service.FromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> udpate(@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.FromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/jgmonteiro/course/services/UserService.java
+++ src/main/java/com/jgmonteiro/course/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jgmonteiro.course.domain.User;
+import com.jgmonteiro.course.dto.UserDTO;
 import com.jgmonteiro.course.repository.UserRepository;
 import com.jgmonteiro.course.services.exception.ObjectNotFoundException;
 
@@ -17,6 +18,7 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	
 	public List<User> findAll(){
 		return repository.findAll();
 	}
@@ -25,4 +27,33 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id); 
+		repository.deleteById(id);
+		 
+	}
+	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return repository.save(newUser);
+	}
+	
+	
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+
+	public User FromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 }
