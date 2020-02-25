1eaea239297aed56d0fd24194d4e2357a50d853e
João Gabriel <jgmoreira15@gmail.com>
2020-02-25 20:09:59 -0300

    Criação da entidade Post com User aninhado

--- src/main/java/com/jgmonteiro/course/config/Instantiation.java
+++ src/main/java/com/jgmonteiro/course/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.jgmonteiro.course.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.jgmonteiro.course.domain.Post;
 import com.jgmonteiro.course.domain.User;
+import com.jgmonteiro.course.repository.PostRepository;
 import com.jgmonteiro.course.repository.UserRepository;
 
 @Configuration
@@ -16,18 +20,27 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
-	
+	@Autowired
+	private PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User viuva = new User(null, "Viuva Negra", "viuvan@gmail.com");
 		User thor = new User(null, "Thor", "thor@gmail.com");
 		User invernal = new User(null, "Soldado Invernal", "soldadoin@gmail.com");
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", thor);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", thor);
+		
 		userRepository.saveAll(Arrays.asList(viuva,thor,invernal));
+		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 	}
 
