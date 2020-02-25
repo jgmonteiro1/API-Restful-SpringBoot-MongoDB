bcd2730ee8d8336160969bd0867ee68806c16f3e
João Gabriel <jgmoreira15@gmail.com>
2020-02-25 20:55:13 -0300

    Endpoint para retornar os posts pelo ID de um user

--- src/main/java/com/jgmonteiro/course/resources/UserResource.java
+++ src/main/java/com/jgmonteiro/course/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.jgmonteiro.course.domain.Post;
 import com.jgmonteiro.course.domain.User;
 import com.jgmonteiro.course.dto.UserDTO;
 import com.jgmonteiro.course.services.UserService;
@@ -61,5 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPost());
 	}
 }
